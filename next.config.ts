15f34e40a4de5fef505592a1c51db9b916243592
josephlesterbacs <106429121+jlbbacs@users.noreply.github.com>
2026-01-07 13:52:21 +0800

    wtf

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // This tells Next.js exactly where your project lives
+  // so it stops looking for a 'src' folder
   typescript: {
     ignoreBuildErrors: false,
+  },
+  // If you are on Next.js 16, use this to assist the bundler
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@/lib/*": ["./app/lib/*"],
+        "@/action/*": ["./app/action/*"],
+      },
+    },
   },
 };
 
